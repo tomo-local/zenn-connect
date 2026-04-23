9e565b11bbe7dbb70a7ef66c516c5f303d80f6df
tomo-local <tomohiro.togashi@gmail.com>
2026-04-23 23:03:42 +0900

    feat: add script to create new repo and register as submodule

---
+++ scripts/submodule-add.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# 新しい GitHub リポジトリを作成して submodule として追加する
+#
+# 使い方:
+#   ./scripts/submodule-add.sh <repo-name> [description]
+#
+# 例:
+#   ./scripts/submodule-add.sh react-mini-redux "Zenn記事: Reduxを自作"
+
+set -e
+
+REPO_NAME="$1"
+DESCRIPTION="${2:-}"
+GITHUB_USER="tomo-local"
+
+if [ -z "$REPO_NAME" ]; then
+  echo "Usage: $0 <repo-name> [description]"
+  exit 1
+fi
+
+if [ -d "repos/$REPO_NAME" ]; then
+  echo "Error: repos/$REPO_NAME already exists."
+  exit 1
+fi
+
+echo "Creating GitHub repository: $GITHUB_USER/$REPO_NAME"
+gh repo create "$GITHUB_USER/$REPO_NAME" \
+  --public \
+  --description "$DESCRIPTION" \
+  --add-readme
+
+echo "Adding as submodule..."
+git submodule add "https://github.com/$GITHUB_USER/$REPO_NAME" "repos/$REPO_NAME"
+
+git add .gitmodules "repos/$REPO_NAME"
+git commit -m "feat: add submodule $REPO_NAME"
+
+echo ""
+echo "Done. repos/$REPO_NAME has been added."
+echo "Repo: https://github.com/$GITHUB_USER/$REPO_NAME"
